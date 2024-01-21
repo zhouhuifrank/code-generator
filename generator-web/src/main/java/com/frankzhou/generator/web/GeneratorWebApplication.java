357f5f797f3a1e4b3c1ea937f6f13a43f850a916
frankzhou <2280610818@qq.com>
2024-01-21 14:21:42 +0800

    项目框架搭建

--- generator-web/src/main/java/com/frankzhou/generator/web/GeneratorWebApplication.java
+++ generator-web/src/main/java/com/frankzhou/generator/web/GeneratorWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeneratorWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeneratorWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorWebApplication.class, args);
+    }
 
 }
