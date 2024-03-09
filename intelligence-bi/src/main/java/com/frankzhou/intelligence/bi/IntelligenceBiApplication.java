8b335cccd8c623887b83209ec8be3ea678155f2f
frankzhou <2280610818@qq.com>
2024-03-09 20:55:28 +0800

    智能BI项目初始化

---
+++ intelligence-bi/src/main/java/com/frankzhou/intelligence/bi/IntelligenceBiApplication.java
@@ -0,0 +1,13 @@
+package com.frankzhou.intelligence.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligenceBiApplication.class, args);
+	}
+
+}
---
+++ intelligence-bi/src/main/java/com/frankzhou/intelligence/bi/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.frankzhou.intelligence.bi.controller;
+
+/**
+ * @author This.FrankZhou
+ * @version 1.0
+ * @description
+ * @date 2024-03-01
+ */
+public class UserController {
+}
